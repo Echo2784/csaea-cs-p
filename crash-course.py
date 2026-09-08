0499cdbff82d273da119a9d6b5c2a32d247d9b35
Echo2784 <echorxd@icloud.com>
2026-09-08 18:22:24 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,28 @@
+# comment
+
+print("hello world")
+# VARIABLE DECLARATIONS AND DATA TYPES:
+a = 4           # integer
+b = 5.5         # float
+c = "CSAEA"     # string
+d = False       # boolean
+
+print(a, b, c, d)
+
+# OPERATIONS
+# + - / *   % ** //
+# +=   -=   /=
+
+e = 3 - 1
+print(e)
+e += 7
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 7
+e += 12
+
+print(f"e is NOW equal to {e}")
