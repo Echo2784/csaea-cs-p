06dff2c5de8acd6083ce4965af03aeeb0c8ddf34
Echo2784 <echorxd@icloud.com>
2026-09-10 16:26:41 +0000

    crashcourse day 2 complete

--- crash-course.py
+++ crash-course.py
@@ -1,28 +1,85 @@
+import math
+
 # comment
 
-print("hello world")
-# VARIABLE DECLARATIONS AND DATA TYPES:
-a = 4           # integer
-b = 5.5         # float
-c = "CSAEA"     # string
-d = False       # boolean
+# to comment multiple lines,
+# highlight the lines,
+# and click ctrl + / 
 
+print("Hello world!")
+
+# VARIABLE DECLARATIONS AND DATA TYPES:
+
+a = 4           # integer 
+b = 5.5         # float 
+c = "CSAEA"     # string
+d = False       # boolean 
+
+print(a)
 print(a, b, c, d)
 
-# OPERATIONS
-# + - / *   % ** //
-# +=   -=   /=
+# MATH OPERATORS 
+# + - / *    %  **  //
+# +=   -=   /=   
 
 e = 3 - 1
 print(e)
-e += 7
+e += 20
 print(e)
 
-# f-string
+# f-string, formatted strings
 
 print(f"e is equal to {e}")
 
-e -= 7
+e -= 7 
 e += 12
 
 print(f"e is NOW equal to {e}")
+
+# COMPARISONS (booleans, which always return True of False)
+
+#  <   >    <=   >=    ==    !=
+
+print(5 <=  5)
+print(7 == 4)
+print(1 != 2)
+
+isEqual = "Yes" != "YES"
+print(isEqual)
+
+# LOGICAL OPERATORS
+# In order of precedence: not   and    or
+
+f = False
+t = True
+
+#predict output.  Don't run the code
+print(not f) # True
+print(f and t) # False
+print(f or t) # True
+print(f or t and not f) # True
+
+#or: tf=t, ft=t, tt=t, ff=f
+#and: tf=f, ft=f, tt=t, ff=f
+
+# CASTING ()
+
+g = int(5.6536539)
+h = str(4)
+print(g,h)
+
+# STRINGS
+
+s1 = "Goodnight"
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2 + s3
+end += ", Cowboy."
+print(end + "\n")
+# MATH LIBRARY
+# max, min, square root
+
+print(math.sqrt(14))
+print(math.ceil(3.65))
+print(math.floor(8.94))
+print(math.pow(2,4))
